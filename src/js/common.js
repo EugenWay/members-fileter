cf7c6aece4612a6d04bbbdbcd48e3610db1cecaf
Eugene Way <eugeneway@MBP-Eugene.home>
2019-07-05 13:42:52 +0200

    add func getMembers

--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,29 @@
-const test = (...args) => console.log(args);
-test(2,4,4);
+import { get } from "https";
+
+
+const getMembers = (number) => {
+    return new Promise((resolve, reject) =>{
+        const api = `https://api.randomuser.me/?nat=US&results=${number}`;
+        const xhr = new XMLHttpRequest();
+
+        xhr.open('GET', api);
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                resolve(JSON.parse(xhr.response).results);
+            } else {
+                reject(Error(xhr.statusText));
+            }
+        }
+        xhr.onerror = (error) => rejects(error);
+        xhr.send();
+
+    });
+}
+
+getMembers(10)
+    .then(users => {
+        console.info(`Success, all users downloaded!`);
+        console.log(users);
+    })
+    .catch(error => console.error(error));
+    
