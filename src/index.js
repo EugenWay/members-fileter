a1d0ec455b452484b3c76350faa15350164b733a
Eugene Way <eugeneway@MBP-Eugene.home>
2019-07-16 11:54:09 +0200

    change XHR to fetch metod, rename getMembers func tu GetUsers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import '@babel/polyfill'
 import './js/common'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/main.css'
--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,4 @@
-import { get } from "https";
+
 
 const input = document.getElementById('request-input');
 const submit = document.getElementById('request-button');
@@ -11,35 +11,25 @@ submit.onclick = () => {
 
     let number = input.value;
 
-    getMembers(number)
-    .then(users => {
-        console.info(`Success, all users downloaded!`);
-        console.log(users);
-        return users;
-    })
-    .then(displayUsers)
-    .catch(error => console.error(error))
+    getUsers(number)
+        .then(users => {
+            console.info(`Success, all users downloaded!`);
+            console.log(users);
+            return users;
+        })
+        .then(displayUsers)
+        .catch(error => console.error(error))
 
 }
 
-const getMembers = (number) => {
-    return new Promise((resolve, reject) =>{
-        const api = `https://api.randomuser.me/?nat=US&results=${number}`;
-        const xhr = new XMLHttpRequest();
+const getUsers = async (number) => {
+    const response =  await fetch(`https://api.randomuser.me/?nat=US&results=${number}`)
+    const data = await response.json()
+    return data.results;
 
-        xhr.open('GET', api);
-        xhr.onload = () => {
-            if (xhr.status === 200) {
-                resolve(JSON.parse(xhr.response).results);
-            } else {
-                reject(Error(xhr.statusText));
-            }
-        }
-        xhr.onerror = (error) => rejects(error);
-        xhr.send();
-
-    });
 }
+
+
 
 const displayUsers = (users) => {
 
